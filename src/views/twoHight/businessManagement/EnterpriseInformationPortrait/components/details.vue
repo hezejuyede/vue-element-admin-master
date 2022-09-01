1ecdf7f887e08f02070e3d6bce216d0b558d60a9
qieerxi <1574408801@qq.com>
2022-09-01 09:23:51 +0800

    9dian23fen

--- src/views/twoHight/businessManagement/EnterpriseInformationPortrait/components/details.vue
+++ src/views/twoHight/businessManagement/EnterpriseInformationPortrait/components/details.vue
@@ -22,7 +22,7 @@ export default {
         { 'img': require('../images/icon2.png'), 'name': '统一社会信用代码', 'number': '7' },
         { 'img': require('../images/icon3.png'), 'name': '所属城市', 'number': '7' },
         { 'img': require('../images/icon4.png'), 'name': '所属县区', 'number': '7' },
-        { 'img': require('../images/icon5.png'), 'name': '企业详细地址', 'number': '7' },
+        { 'img': require('../images/icon5.png'), 'name': '企业详细地址', 'number': '济南市历下区龙鼎大道1号龙奥大厦5层济南市历下区龙鼎大道1号龙奥大厦5层' },
         { 'img': require('../images/icon6.png'), 'name': '地理位置', 'number': '7' },
         { 'img': require('../images/icon7.png'), 'name': '企业性质', 'number': '7' },
         { 'img': require('../images/icon8.png'), 'name': '行业归属', 'number': '7' },
@@ -64,30 +64,45 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-top: 15px;
+    margin-top: 10px;
     .bottomTL{
       flex:1;
       height: 100%;
       display: flex;
       align-items: center;
-      justify-content: flex-end;
+      justify-content: flex-start;
+      padding-left: 20px;
     }
     .bottomTR{
-      flex: 1;
+      flex: 3;
       height: 100%;
       display: flex;
       align-items: flex-start;
       justify-content: center;
       flex-direction: column;
-      padding-left: 10px;
       div:first-child{
+        flex: 1.1;
+        width: 100%;
+        height: 100%;
         color: #A6AAB0;
         font-size: 14px;
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-start;
+        padding-bottom: 2px;
       }
       div:last-child{
+        flex: 1.4;
+        width: 100%;
+        height: 100%;
         color: #283650;
-        font-size: 24px;
+        font-size: 14px;
         font-weight: bold;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display:-webkit-box;
+        -webkit-box-orient:vertical;
+        -webkit-line-clamp:2;
       }
     }
   }
--- src/views/twoHight/businessManagement/EnterpriseInformationPortrait/components/list.vue
+++ src/views/twoHight/businessManagement/EnterpriseInformationPortrait/components/list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="visibleDiv">
     <div class="visibleDivSelect">
-      <label style="margin-right: 5px;margin-left: 5px" class="fl">
+      <label style="margin-right: 5px" class="fl">
         <el-date-picker style="width: 240px" v-model="examineTime" type="daterange"
                         start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd">
         </el-date-picker>
@@ -15,15 +15,14 @@
     <div class="visibleTable">
       <el-table class="tb-edit" :data="tableData"
                 :header-cell-style="{background:'#EDF4F4',color:'#474F4F',height:'40px',borderColor:'#CAE5E4',fontSize:'14px',fontWeight: 'bold'}"
-                :cell-style="{fontSize:'12px',fontWeight: 'norma',color:'#444B4B',background:'#FFFFFF',borderColor:'#CAE5E4'}"
+                :cell-style="{fontSize:'12px',fontWeight: 'norma',color:'#444B4B',background:'#FFFFFF',borderColor:'#CAE5E4',fontWeight: 'bold'}"
                 border
-                :height="200"
                 ref="moviesTable"
                 highlight-current-row style="width: 95%;margin: auto">>
-        <el-table-column label="信息项" prop="jssj" align="center"></el-table-column>
-        <el-table-column label="数值" prop="jssj" align="center"></el-table-column>
-        <el-table-column label="信息项" prop="jssj" align="center"></el-table-column>
-        <el-table-column label="数值" prop="sdzt" align="center"></el-table-column>
+        <el-table-column label="信息项" prop="xx1" align="center"></el-table-column>
+        <el-table-column label="数值" prop="value1" align="center"></el-table-column>
+        <el-table-column label="信息项" prop="xx2" align="center"></el-table-column>
+        <el-table-column label="数值" prop="value2" align="center"></el-table-column>
       </el-table>
     </div>
   </div>
@@ -35,7 +34,15 @@ export default {
   name: 'List',
   data() {
     return {
-      tableData: [],
+      tableData: [
+        { 'xx1': '综合能源消费量', 'value1': '', 'xx2': '煤炭消费量', 'value2': '' },
+        { 'xx1': '电力', 'value1': '', 'xx2': '油品', 'value2': '' },
+        { 'xx1': '天然气', 'value1': '', 'xx2': '热量', 'value2': '' },
+        { 'xx1': '其他', 'value1': '', 'xx2': '用水量', 'value2': '' },
+        { 'xx1': '产品', 'value1': '', 'xx2': '产量', 'value2': '' },
+        { 'xx1': '产值', 'value1': '', 'xx2': '主要营业收入', 'value2': '' },
+        { 'xx1': '利润', 'value1': '', 'xx2': '入库税', 'value2': '' }
+      ],
       examineTime: [],
     }
   },
@@ -51,6 +58,17 @@ export default {
   props: {}
 }
 </script>
+<style lang="scss">
+.el-table {
+  th {
+    padding: 0 ;
+  }
+  td {
+    padding: 5px ;
+  }
+}
+
+</style>
 <style lang="scss" scoped>
 
 .visibleDiv {
